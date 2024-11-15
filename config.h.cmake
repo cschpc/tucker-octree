2dd3b24ef996812672d5bc2bc36326789e959103
Juhani Kataja <juhani.kataja@csc.fi>
2024-11-15 16:40:10 +0200

    added missing config.h.cmake

---
+++ config.h.cmake
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#cmakedefine VDF_REAL_DTYPE @VDF_REAL_DTYPE@
+#cmakedefine OCTREE_TUCKER_CORE_RANK @OCTREE_TUCKER_CORE_RANK@
+#cmakedefine MAX_ROOT_DIMS
+#cmakedefine OCTREE_VIEW_INDEX_TYPE
+
+#endif // CONFIG_H
