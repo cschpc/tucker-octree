f6ed4a0df49b2897768dd5efcda96e09edb37447
Juhani Kataja <juhani.kataja@csc.fi>
2024-12-17 14:25:59 +0200

    improved license text and authorship

--- zfp_iface.hpp
+++ zfp_iface.hpp
@@ -1,3 +1,28 @@
+/*
+ * Tucker-Octree multiresolution voxel data compression library
+ * Copyright (C) 2024 CSC - IT Center for Science Ltd
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+/* 
+ * Authors: Juhani Kataja (*), Konstantinos Papadakis (#)
+ * Affiliation: CSC - IT Center for Science Ltd (*), Universy of Helsinki (#)
+ * Email: juhani.kataja@csc.fi
+ */
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <array>
